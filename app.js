764b5f4115e31ed3b2489ebae171b4b60cc9433a
sowon <thdnjs3596@gmail.com>
2022-10-02 00:19:42 +0900

    * 배경음악 추가

--- app.js
+++ app.js
@@ -8,18 +8,39 @@ const audioBird = new Audio("sound/bird.mp3");
 audioBird.volume = 0.5;
 audioBird.loop = true;
 
-const play = document.querySelector(".play");
+const audioGarden = new Audio("sound/bg_garden.mp3");
+audioGarden.volume = 0.5;
+audioGarden.loop = true;
+const audioHappy = new Audio("sound/bg_happy.mp3");
+audioHappy.volume = 0.5;
+audioHappy.loop = true;
+const audioRain = new Audio("sound/bg_rain.mp3");
+audioRain.volume = 0.5;
+audioRain.loop = true;
+const audioSunny = new Audio("sound/bg_sunny.mp3");
+audioSunny.volume = 0.5;
+audioSunny.loop = true;
+
 const dog = document.querySelector(".dog");
 const cat = document.querySelector(".cat");
 const bird = document.querySelector(".bird");
+const bg_garden = document.querySelector(".bg_garden");
+const bg_happy = document.querySelector(".bg_happy");
+const bg_rain = document.querySelector(".bg_rain");
+const bg_sunny = document.querySelector(".bg_sunny");
 const volume_slider = document.querySelector(".volume_slider");
 
 const DOG = 'dog';
 const CAT = 'cat';
 const BIRD = 'bird';
+const BG_GARDEN = 'bg_garden';
+const BG_HAPPY = 'bg_happy';
+const BG_RAIN = 'bg_rain';
+const BG_SUNNY = 'bg_sunny';
 const ON = 'on';
 const OFF = 'off';
 
+let bg = '';
 let animals = [];
 
 function playSound(animal) {
@@ -30,11 +51,75 @@ function playSound(animal) {
   }
 }
 
-function pauseSount(animal) {
+function playBgSound(bgName) {
+  switch (bg) {
+    case BG_GARDEN :
+      bg_garden.classList.remove(ON);
+      bg_garden.classList.add(OFF);
+      break;
+    case BG_HAPPY :
+      bg_happy.classList.remove(ON);
+      bg_happy.classList.add(OFF);
+      break;
+    case BG_RAIN :
+      bg_rain.classList.remove(ON);
+      bg_rain.classList.add(OFF);
+      break;
+    case BG_SUNNY :
+      bg_sunny.classList.remove(ON);
+      bg_sunny.classList.add(OFF);
+      break;
+  }
+  if(bg != bgName) {
+    switch (bgName) {
+      case BG_GARDEN :
+        bg_garden.classList.toggle(ON);
+        bg_garden.classList.toggle(OFF);
+        break;
+      case BG_HAPPY :
+        bg_happy.classList.toggle(ON);
+        bg_happy.classList.toggle(OFF);
+        break;
+      case BG_RAIN :
+        bg_rain.classList.toggle(ON);
+        bg_rain.classList.toggle(OFF);
+        break;
+      case BG_SUNNY :
+        bg_sunny.classList.toggle(ON);
+        bg_sunny.classList.toggle(OFF);
+        break;
+    }
+    }
+
+  pauseBgSound();
+
+  if(bg != bgName) { 
+    bg = bgName;
+    
+    switch (bgName) {
+      case BG_GARDEN : audioGarden.play(); break;
+      case BG_HAPPY : audioHappy.play(); break;
+      case BG_RAIN : audioRain.play(); break;
+      case BG_SUNNY : audioSunny.play(); break;
+    }
+  } else if (bg == bgName) bg = '';
+
+}
+
+function pauseSound(animal) {
   switch (animal) {
     case DOG : audioDog.pause(); break;
     case CAT : audioCat.pause(); break;
     case BIRD : audioBird.pause(); break;
+  }
+}
+
+function pauseBgSound() {
+  switch (bg) {
+    case BG_GARDEN : audioGarden.pause(); break;
+    case BG_HAPPY : audioHappy.pause(); break;
+    case BG_RAIN : audioRain.pause(); break;
+    case BG_SUNNY : audioSunny.pause(); break;
   }
 }
 
@@ -51,7 +136,7 @@ function setOnOffAni(animal, className){
 
   console.log(animals)
   if(className == ON) playSound(animal);
-  else pauseSount(animal);
+  else pauseSound(animal);
 
   switch (animal) {
     case DOG :
@@ -73,17 +158,40 @@ function setVolume() {
   audioDog.volume = volume_slider.value / 100;
   audioCat.volume = volume_slider.value / 100;
   audioBird.volume = volume_slider.value / 100;
+  audioGarden.volume = volume_slider.value / 100;
+  audioHappy.volume = volume_slider.value / 100;
+  audioRain.volume = volume_slider.value / 100;
+  audioSunny.volume = volume_slider.value / 100;
 }
 
 dog.addEventListener("click", function () {
-  if (animals.includes(DOG)) setOnOffAni(DOG,OFF);  else setOnOffAni(DOG,ON);
+  if (animals.includes(DOG)) setOnOffAni(DOG,OFF);  
+  else setOnOffAni(DOG,ON);
 });
 
 cat.addEventListener("click", function () {
-  if (animals.includes(CAT)) setOnOffAni(CAT,OFF);  else setOnOffAni(CAT,ON);
+  if (animals.includes(CAT)) setOnOffAni(CAT,OFF); 
+  else setOnOffAni(CAT,ON);
 });
 
 bird.addEventListener("click", function () {
-  if (animals.includes(BIRD)) setOnOffAni(BIRD,OFF);  else setOnOffAni(BIRD,ON);
+  if (animals.includes(BIRD)) setOnOffAni(BIRD,OFF);  
+  else setOnOffAni(BIRD,ON);
+});
+
+bg_garden.addEventListener("click", function () {
+  playBgSound(BG_GARDEN); 
+});
+
+bg_happy.addEventListener("click", function () {
+  playBgSound(BG_HAPPY); 
+});
+
+bg_rain.addEventListener("click", function () {
+  playBgSound(BG_RAIN); 
+});
+
+bg_sunny.addEventListener("click", function () {
+  playBgSound(BG_SUNNY); 
 });
 
